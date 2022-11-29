f287536ebeab46c20cc67e2ca224222707fb3fa2
Chiichriis <chichris28@gmail.com>
2022-11-29 14:54:29 -0800

    work done

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - James Chiedozie James
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - James Chiedozie James
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
