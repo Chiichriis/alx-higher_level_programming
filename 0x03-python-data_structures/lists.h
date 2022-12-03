1f3d42a3efb97cb4eccbf5cc8126bc609a289eab
Chiichriis <chichris28@gmail.com>
2022-12-03 12:53:06 -0800

    tasking

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -15,8 +15,8 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+  int n;
+  struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -26,4 +26,4 @@ void free_listint(listint_t *head);
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H *
+#endif /* LISTS_H */
