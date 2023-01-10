eecdac3dfe6916b4ca0589de0fd788c03db477b5
Chiichriis <chichris28@gmail.com>
2023-01-10 07:58:42 +0100

    completed

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''module for task 0'''
+
+
+def read_file(filename=""):
+    '''reads a file in utf-8 encoding'''
+    with open(filename, encoding='utf-8') as p:
+        txt = p.read()
+    print(txt, end='')
